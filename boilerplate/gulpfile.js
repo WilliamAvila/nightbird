152ae3fd492e9d7dc5ea7129988972bc0bed013d
siwady <siwady0908@gmail.com>
2016-02-18 13:51:59 -0600

    opening localhost:3000 when running tests

--- boilerplate/gulpfile.js
+++ boilerplate/gulpfile.js
@@ -1,3 +1,4 @@
+var os = require('os');
 var gulp = require('gulp');
 var gutil = require("gulp-util");
 var webpack = require("webpack");
@@ -6,9 +7,17 @@ var webpackDevConfig = require("./webpack.config.js");
 var webpackProdConfig = require("./webpack.prod.config.js");
 var webserver = require('gulp-webserver');
 var karmaServer = require('karma').Server;
+var open = require('gulp-open');
 
-gulp.task('default', ['dev','tdd']);
-
+gulp.task('default', ['dev', 'tdd','browser'], function () {
+});
+gulp.task('browser', function () {
+    var browser = os.platform() === 'linux' ? 'google-chrome' : (
+        os.platform() === 'darwin' ? 'google chrome' : (
+            os.platform() === 'win32' ? 'chrome' : 'firefox'));
+    gulp.src('')
+        .pipe(open({ app: browser, uri: 'http://localhost:3000' }));
+});
 gulp.task('build_prod', function (callback) {
     // modify some webpack config options
     var myConfig = Object.create(webpackProdConfig);
@@ -64,10 +73,10 @@ gulp.task('prod', ['build_prod'], function (callback) {
         }));
 });
 
-gulp.task('tdd',function (done) {
-  new karmaServer({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: false,
-    autoWatch:true
-  }, done).start();
+gulp.task('tdd', function (done) {
+    new karmaServer({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: false,
+        autoWatch: true
+    }, done).start();
 });
