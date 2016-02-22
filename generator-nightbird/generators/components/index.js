7dc49911cede5f5bdfd06e87d2573bdedcfc40f9
Saul Mayorquin <saulmadi@gmail.com>
2016-02-22 12:31:56 -0600

    fixing current path for component

--- generator-nightbird/generators/components/index.js
+++ generator-nightbird/generators/components/index.js
@@ -7,43 +7,48 @@ var _ = require('lodash');
 
 
 module.exports = yeoman.generators.Base.extend({
-    
-    
+
+
     prompting: function () {
         var done = this.async();
-            
-            var promtTitle = {
-                type: 'input',
-                name: 'title',
-                message: 'Component Title (home)',
-                default: 'home'
-            };
-            
-            var prompts = [promtTitle];
-        
-            this.prompt(prompts, function (props) {
-                 this.props = props;
-                 this.metadata =  {
-                   title : _.kebabCase(this.props.title),
-                   classtitle : _.capitalize(this.props.title)
-               }
-                 done();
-            }.bind(this));
+
+        var promtTitle = {
+            type: 'input',
+            name: 'title',
+            message: 'Component Title (home)',
+            default: 'home'
+        };
+
+        var prompts = [promtTitle];
+
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            this.metadata = {
+                title: _.kebabCase(this.props.title),
+                classtitle: _.capitalize(this.props.title)
+            }
+            done();
+        }.bind(this));
     },
 
-    writing:{
-        
-        service:function(){
-               var template = this.templatePath('component');
-               var destination = this.destinationPath('src/app/home/components/'+this.metadata.title+'.ts');                
-               this.fs.copyTpl(template,destination,this.metadata);         
+    writing: {
+
+        service: function () {
+            var template = this.templatePath('component');
+            
+            var currentDirectory = this.destinationRoot();
+            
+            var destination = this.destinationPath(currentDirectory + '/components/' + this.metadata.title + '.ts');
+            this.fs.copyTpl(template, destination, this.metadata);
         },
-        spec:function(){
-               var template = this.templatePath('component.spec');
-               var destination = this.destinationPath('src/specs/home/components/'+this.metadata.title+'.spec.ts'); 
-               this.fs.copyTpl(template,destination,this.metadata); 
+        spec: function () {
+            var currentDirectory = this.destinationRoot();
+            var specDirectory = currentDirectory.replace('app', 'specs');
+            var template = this.templatePath('component.spec');
+            var destination = this.destinationPath(specDirectory + '/components/' + this.metadata.title + '.spec.ts');
+            this.fs.copyTpl(template, destination, this.metadata);
         }
-        
+
     },
 
     install: function () {
