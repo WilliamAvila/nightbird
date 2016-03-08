0d800ca87810cf08b331d12ba0631bf49878cb53
saulmadi <saulmadi@gmail.com>
2016-03-08 14:39:11 -0600

    editorconfig and folder helper

--- generator-nightbird/generators/component/index.js
+++ generator-nightbird/generators/component/index.js
@@ -9,56 +9,56 @@ var _ = require('lodash');
 module.exports = yeoman.generators.Base.extend({
 
 
-    prompting: function () {
-        var done = this.async();
+		prompting: function () {
+				var done = this.async();
 
-        var promtTitle = {
-            type: 'input',
-            name: 'title',
-            message: 'Component Title (home)',
-            default: 'home'
-        };
-        
-        var promtSelector = {
-            type: 'input',
-            name: 'selector',
-            message: 'Component Selector (aa-home)',
-            default: 'aa-home'
-        };
+				var promtTitle = {
+						type: 'input',
+						name: 'title',
+						message: 'Component Title (home)',
+						default: 'home'
+				};
 
-        var prompts = [promtTitle,promtSelector];
+				var promtSelector = {
+						type: 'input',
+						name: 'selector',
+						message: 'Component Selector (aa-home)',
+						default: 'aa-home'
+				};
 
-        this.prompt(prompts, function (props) {
-            this.props = props;
-            this.metadata = {
-                title: _.kebabCase(this.props.title),
-                classtitle: _.capitalize(this.props.title),
-                selector: _.kebabCase(this.props.selector)
-            }
-            done();
-        }.bind(this));
-    },
+				var prompts = [promtTitle,promtSelector];
 
-    writing: {
+				this.prompt(prompts, function (props) {
+						this.props = props;
+						this.metadata = {
+								title: _.kebabCase(this.props.title),
+								classtitle: _.capitalize(this.props.title),
+								selector: _.kebabCase(this.props.selector)
+						}
+						done();
+				}.bind(this));
+		},
 
-        service: function () {
-            var template = this.templatePath('component');
-            
-            var currentDirectory = this.destinationRoot();
-            
-            var destination = this.destinationPath(currentDirectory + '/components/' + this.metadata.title + '.ts');
-            this.fs.copyTpl(template, destination, this.metadata);
-        },
-        spec: function () {
-            var currentDirectory = this.destinationRoot();
-            var specDirectory = currentDirectory.replace('app', 'specs');            
-            var template = this.templatePath('component.spec');
-            var destination = this.destinationPath(specDirectory + '/components/' + this.metadata.title + '.spec.ts');
-            this.fs.copyTpl(template, destination, this.metadata);
-        }
+		writing: {
 
-    },
+				service: function () {
+						var template = this.templatePath('component');
 
-    install: function () {
-    }
+						var currentDirectory = this.destinationRoot();
+						
+						var destination = this.destinationPath(currentDirectory + '/components/' + this.metadata.title + '.ts');
+						this.fs.copyTpl(template, destination, this.metadata);
+				},
+				spec: function () {
+						var currentDirectory = this.destinationRoot();
+						var specDirectory = currentDirectory.replace('app', 'specs');            
+						var template = this.templatePath('component.spec');
+						var destination = this.destinationPath(specDirectory + '/components/' + this.metadata.title + '.spec.ts');
+						this.fs.copyTpl(template, destination, this.metadata);
+				}
+
+		},
+
+		install: function () {
+		}
 });
---
+++ generator-nightbird/helpers/folderSelector.js
@@ -0,0 +1,80 @@
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');;
+
+function findDirectoryRecursive(currentPath, pathToFind) {
+  var folders = getDirectories(currentPath);
+  var valueToReturn = null;
+
+  for (var index = 0; index < folders.length; index++) {
+    var value = folders[index];
+
+    if (pathToFind === value) {
+      return currentPath + '/' + value;
+    } else {
+      return findDirectoryRecursive(currentPath + '/' + value, pathToFind);
+    }
+
+  }
+
+}
+
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
+}
+
+function createFoldersInAppDirectory(appDirectory) {
+  var folders = getDirectories(appDirectory);
+  return _.map(folders, function creatRoutAndName(folder) {
+    return { name: folder, path: appDirectory + '/' + folder }
+  })
+}
+
+function findAllFoldersInApp(callback) {
+  var response = {
+    err: null,
+    directories: []
+  };
+  var currentPath = __dirname;
+  var appDirectory = __dirname + '/src/app';
+
+  fs.accessSync
+  fs.access(appDirectory, fs.F_OK, function(err) {
+
+    if (!err) {
+      response.directories = createFoldersInAppDirectory(appDirectory);
+      callback(response)
+
+    } else {
+      appDirectory = findDirectoryRecursive(currentPath, 'app');
+
+      if (appDirectory) {
+        response.directories = createFoldersInAppDirectory(appDirectory);
+        callback(response)
+
+      } else {
+        response.err = 'Could\'nt find the app directory'
+        callback(response)
+      }
+
+    }
+  });
+
+}
+
+var folderSelector = {
+  findAllFoldersInApp: findAllFoldersInApp,
+
+  findDirectoryRecursive: findDirectoryRecursive,
+
+  getDirectories: getDirectories
+}
+
+console.log('log del directorio' + __dirname);
+folderSelector.findAllFoldersInApp(function respones(res) {
+  console.log('Retorna valores: ' + JSON.stringify(res));
+})
+
+module.exports = folderSelector;
