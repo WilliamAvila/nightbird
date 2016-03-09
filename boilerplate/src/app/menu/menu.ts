1b20a50c1701765c07a13b08a5c8b96ec6c58cdc
siwady <siwady0908@gmail.com>
2016-03-09 14:38:32 -0600

    adding example of nav menu

---
+++ boilerplate/src/app/menu/menu.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'aa-menu',
+    template: require('./aa-menu.html'),
+    styles: [require('./menu.scss')]
+})
+export class Menu {
+    constructor() { }
+}
