6215f5fd1e5f8aa3538b7be9cf9ea364a0957bbc
siwady <siwady0908@gmail.com>
2016-03-11 10:56:55 -0600

    adding validation forms

--- boilerplate/src/app/users/component/create/create.ts
+++ boilerplate/src/app/users/component/create/create.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {} from 'angular2/forms';
 import {User} from '../../user';
-import {FORM_DIRECTIVES, FormBuilder, ControlGroup } from 'angular2/common';
+import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators} from 'angular2/common';
 
 @Component({
     selector: 'user-create',
@@ -14,11 +14,13 @@ export class CreateUser implements OnInit, OnDestroy {
     createForm: ControlGroup;
     constructor(fb: FormBuilder) {
         this.createForm = fb.group({
-            'name': [''],
-            'username': [''],
-            'email': [''],
-            'phone': [''],
-            'website': [''],
+            'name': ['', Validators.compose([Validators.required,
+                Validators.nullValidator, Validators.minLength(1), Validators.maxLength(30)])],
+            'username': ['', Validators.compose([Validators.required,
+                Validators.nullValidator, Validators.minLength(1), Validators.maxLength(30)])],
+            'email': ['', Validators.compose([Validators.required, Validators.nullValidator])],
+            'phone': ['', Validators.maxLength(15)],
+            'website': ['', Validators.maxLength(30)],
         });
     }
     ngOnInit() {
