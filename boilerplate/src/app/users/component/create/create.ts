775628a2e02f81c79977ca60565c28746035d4d8
siwady <siwady0908@gmail.com>
2016-03-11 10:40:56 -0600

    adding formbuilder on create form

--- boilerplate/src/app/users/component/create/create.ts
+++ boilerplate/src/app/users/component/create/create.ts
@@ -1,16 +1,31 @@
 import {Component, OnInit, OnDestroy} from 'angular2/core';
+import {} from 'angular2/forms';
 import {User} from '../../user';
+import {FORM_DIRECTIVES, FormBuilder, ControlGroup } from 'angular2/common';
 
 @Component({
     selector: 'user-create',
     template: require('./create.html'),
-    styles: [require('./create.scss')]
+    styles: [require('./create.scss')],
+    directives: [FORM_DIRECTIVES]
 })
 
 export class CreateUser implements OnInit, OnDestroy {
-    constructor() { }
+    createForm: ControlGroup;
+    constructor(fb: FormBuilder) {
+        this.createForm = fb.group({
+            'name': [''],
+            'username': [''],
+            'email': [''],
+            'phone': [''],
+            'website': [''],
+        });
+    }
     ngOnInit() {
     }
     ngOnDestroy() {
+    }
+    createUser(value: string) {
+        console.log('you submitted value: ', value);
     }
 }
